fcd60f6e74ca145a882bf87b83dc9cf89a0c7247
Jordan D <degco44@gmail.com>
2022-07-17 18:51:08 -0500

    add file paths to server.js

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ const express = require('express');
 const path = require('path');
 const { application } = require('express');
 const app = express();
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('./public'));
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true}));
+// parse incoming JSON data
+app.use(express.json());
+
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
