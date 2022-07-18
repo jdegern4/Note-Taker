7494c6ba97db7780c1673f86c9262df8077570e3
Jordan D <degco44@gmail.com>
2022-07-17 22:16:29 -0500

    fully connect front and back end

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
-const { application } = require('express');
+const { response } = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const db = require('./db/db.json');
 
 app.use(express.static('./public'));
 // parse incoming string or array data
@@ -17,6 +18,29 @@ app.get('/index', (req, res) => {
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// initialize empty array in which to store notes
+let userNotes = [];
+
+function addNote(data) {
+    userNotes.push(data);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({db: userNotes}, null, 2)
+    );
+    return data;
+};
+
+// send stored notes to user
+app.get('/api/notes', (req, res) => {
+    res.json(userNotes);
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = 1; //insert note ID function here
+    const data = req.body;
+    addNote(data);
 });
 
 app.listen(PORT, () => {
